e6b076197fba76da513438c9d0430e7617e868e0
shahroz <shahrozahmad786@gmail.com>
2019-03-26 23:22:47 +0500

    Using methods and function

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -16,12 +16,13 @@
       <div class="row" id="app">
 
             <ul class="list-group offset-4 col-4">
-                  <li class="list-group-item active">Cras justo odio</li>
-                  <li class="list-group-item">Dapibus ac facilisis in</li>
-                  <li class="list-group-item">Morbi leo risus</li>
-                  <li class="list-group-item">Porta ac consectetur ac</li>
-                  <li class="list-group-item">Vestibulum at eros</li>
-                  <input type="text" class="form-control" placeholder="Type your message here...">
+                  <li class="list-group-item active">Chat Room</li>
+                  <message  v-for="value in chat.message">
+
+                       @{{ value }}
+                  </message>
+                  
+                  <input type="text" class="form-control" placeholder="Type your message here..." v-model='message' @keyup.enter='send'>
                 </ul>
       </div>
 </div>
